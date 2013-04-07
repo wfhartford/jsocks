4c821fc19925148f7ca207d4fc161c685d37ae52
Sergey Kurtenko <elbegemot@gmail.com>
2013-04-07 17:54:30 +0300

    Change HashDnsResolver to make less dns lookups

--- src/main/java/com/runjva/sourceforge/jsocks/dns/HashDnsResolver.java
+++ src/main/java/com/runjva/sourceforge/jsocks/dns/HashDnsResolver.java
@@ -3,10 +3,17 @@ package com.runjva.sourceforge.jsocks.dns;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
+import sun.net.util.IPAddressUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Implementation of DnsResolver with custom configured mapping of ip addresses for domains.
+ * It gives ability to config custom mapping like in etc/hosts for each proxy instance.
+ * Resolver first tries take value from configured mapping and uses standard dns lookup as fallback.
+ */
+@SuppressWarnings("restriction")
 public class HashDnsResolver implements DnsResolver {
 	final private static Logger log = LoggerFactory
 			.getLogger(HashDnsResolver.class);
@@ -25,16 +32,25 @@ public class HashDnsResolver implements DnsResolver {
 	@Override
 	public InetAddress resolveByName(String host) throws UnknownHostException {
 		InetAddress address = null;
-		String addressToResolve = host;
+		
 		if (hosts.containsKey(host)) {
-			addressToResolve = hosts.get(host);
-			log.info("resolved from hash ip=" + addressToResolve + " for host="
-					+ host);
+			String ip = hosts.get(host);
+			byte[] ipAsBytes = IPAddressUtil.textToNumericFormatV4(ip);
+			if(ipAsBytes == null){
+				log.warn(String.format("Can't convert string ip=%s to byte array", ip));
+				throw new UnknownHostException("Wrong ip format " + ip);
+			}
+			
+			//create address without lookup
+			address = InetAddress.getByAddress(host, ipAsBytes);				
+			log.info("resolved from hash ip=" + ip + " for host="
+					+ host);					
 		} else {
+			//standard dns lookup as fallback
+			address = InetAddress.getByName(host);
 			log.info("not resolved from hash host=" + host);
 		}
-		address = InetAddress.getByName(addressToResolve);
-
+		
 		return address;
 	}
 }

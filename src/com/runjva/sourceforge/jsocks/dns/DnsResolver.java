9f73d14f8d5b47b2f424de9b4b442d4d9e4334ce
Sergey Kurtenko <elbegemot@gmail.com>
2013-04-07 17:52:56 +0300

    Remove from traking moved files

--- src/com/runjva/sourceforge/jsocks/dns/DnsResolver.java
+++
@@ -1,8 +0,0 @@
-package com.runjva.sourceforge.jsocks.dns;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-public interface DnsResolver {
-	public InetAddress resolveByName(String host) throws UnknownHostException;
-}
--- src/com/runjva/sourceforge/jsocks/protocol/ProxyStatus.java
+++
@@ -1,8 +0,0 @@
-package com.runjva.sourceforge.jsocks.protocol;
-
-/**
- * Enum representing current proxy status
- */
-public enum ProxyStatus {
-	STOPED, STARTED, ERROR
-}
